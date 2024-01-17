23e2f02a9cd9de4e02f509f2183f88ac4124b17d
anOsuPlayer <pelleale261@gmail.com>
2024-01-17 12:25:21 +0100

    added stuff.

--- 6-arraySum/arraySum.s
+++ 6-arraySum/arraySum.s
@@ -3,5 +3,75 @@ S0: .string "%d"
 
 P0: .string "input array length: "
 P1: .string "insert number: "
+P2: .string "the sum of all elements is: %lld"
 
 .section .text
+.globl sumArr
+sumArr:
+pushq %rbp
+movq %rsp, %rbp
+subq $32, %rsp
+
+movq $0, -8(%rbp)
+movl %edx, %edi
+movq $0, %rbx
+L1:
+    movl (%rcx, %rbx, 4), %eax
+    cltq
+    addq %rax, -8(%rbp)
+    incl %ebx
+    cmpl %ebx, %edi
+    jnz L1
+
+movq -8(%rbp), %rax
+addq $32, %rsp
+popq %rbp
+ret
+
+.globl main
+main:
+    pushq %rbp
+    movq %rsp, %rbp
+    subq $32, %rsp
+
+    leaq P0(%rip), %rcx
+    call printf
+    leaq S0(%rip), %rcx
+    leaq -4(%rbp), %rdx
+    call scanf
+
+    movl -4(%rbp), %eax
+    movl $4, %ebx
+    mull %ebx
+    movq %rbp, %rsi
+    addl $8, %eax
+    movl %eax, -8(%rbp)
+    movq %rbp, %rdi
+    subl %eax, %edi
+
+    subl -8(%rbp), %esp
+
+    movl $0, %ebx
+    L0:
+        leaq P1(%rip), %rcx
+        call printf
+        leaq S0(%rip), %rcx
+        leaq (%rdi, %rbx, 4), %rdx
+        call scanf
+        incl %ebx
+        cmpl %ebx, -4(%rbp)
+        jnz L0
+
+    movq %rdi, %rcx
+    movl -4(%rbp), %edx
+    call sumArr
+
+    leaq P2(%rip), %rcx
+    movq %rax, %rdx
+    call printf
+
+    addl -8(%rbp), %esp
+
+    addq $32, %rsp
+    popq %rbp
+    ret
